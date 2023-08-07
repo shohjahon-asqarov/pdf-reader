809f5bf7273458202ac7a69f2f54a8fb5cc8d3ff
Shohjahon <104879567+mr-webprogrammer@users.noreply.github.com>
2023-08-07 20:48:39 +0500

    add base styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 import './serviceWorker.js'
 
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
